5a26bc9698357321f53824347874c7e221b4199d
Rayyy2 <69945345+Rayyy2@users.noreply.github.com>
2022-11-30 18:03:14 +0800

    Update RayyyScript.lua

--- RayyyScript.lua
+++ RayyyScript.lua
@@ -2267,6 +2267,13 @@ if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7
         end)
     end)
 
+    Main:AddButton("Event TEST", function()
+        print("WORK?")
+        for i,v in pairs(workspace.NPCs:GetChildren()) do
+        game:GetService("ReplicatedStorage").Remotes.Celebration:InvokeServer('TalkNpc',workspace.NPCs[v.Name])
+        end
+    end)
+
     Main:AddSeperator("Settings Farm")
 
     Time = Main:AddLabel("Server Time")
